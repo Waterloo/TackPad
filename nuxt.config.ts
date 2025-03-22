c8164684813de9e957f5b2f0d64c5c56c3a686ae
jithin <hello@jithinsha.in>
2025-03-22 10:11:22 +0530

    Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,13 +21,11 @@ export default defineNuxtConfig({
         redirectURL: '...'
       }
     },
-    storage: {
-        bucket: "",
-        endpoint: "",
-        accessKeyId: "",
-        secretAccessKey: "",
-        region: ""
-    }
+        bucket: process.env.NUXT_BUCKET,
+        endpoint: process.env.NUXT_ENDPOINT,
+        accessKeyId: process.env.NUXT_ACCESS_KEY_ID,
+        secretAccessKey: process.env.NUXT_SECRET_ACCESS_KEY,
+        region: process.env.NUXT_REGION
   },
   hub:{
     database:true
