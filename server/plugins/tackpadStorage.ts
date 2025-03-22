a68736e94fc01ee1ef0f4434dd0ee76194bc8188
jithin <hello@jithinsha.in>
2025-03-22 10:20:28 +0530

    update plugin

--- server/plugins/tackpadStorage.ts
+++ server/plugins/tackpadStorage.ts
@@ -5,11 +5,11 @@ export default defineNitroPlugin(() => {
 
   const tackpadStorage = createStorage({
     driver: s3Driver({
-      bucket: process.env.STORAGE_BUCKET!,
-      endpoint: process.env.STORAGE_ENDPOINT!,
-      accessKeyId: process.env.STORAGE_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.STORAGE_SECRET_ACCESS_KEY!,
-      region: process.env.STORAGE_REGION!
+      bucket: useRuntimeConfig().bucket,
+      endpoint: useRuntimeConfig().endpoint,
+      accessKeyId: useRuntimeConfig().accessKeyId,
+      secretAccessKey: useRuntimeConfig().secretAccessKey,
+      region: useRuntimeConfig().region
     })
   })
 
