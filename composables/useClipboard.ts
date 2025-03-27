388e1a3360bb7de55d3cf41d6c42c7c1993dff27
darkwebplayer <amrithanadhm@gmail.com>
2025-03-27 15:51:32 +0530

    open profile on image upload failure

--- composables/useClipboard.ts
+++ composables/useClipboard.ts
@@ -1,33 +1,33 @@
+import { useBoardStore } from "~/stores/board";
+import { useItemManagement } from "./useItemManagement";
+import { useNoteStore } from "~/stores/noteStore";
+import { useTextWidgetStore } from "~/stores/textWidgetStore";
+import { useLinkStore } from "~/stores/linkStore";
+import { useProfileStore } from "~/stores/profileStore";
 
-import { useBoardStore } from '~/stores/board';
-import { useItemManagement } from './useItemManagement';
-import { useNoteStore } from '~/stores/noteStore';
-import { useTextWidgetStore } from '~/stores/textWidgetStore';
-import { useLinkStore } from '~/stores/linkStore';
-
-
-
-async function redirectToLogin(){
-  await navigateTo('/login');
+async function redirectToLogin() {
+  const profileStore = useProfileStore();
+  profileStore.open();
+  profileStore.activeTab = "user";
 }
 export function useClipboard() {
-    const boardStore = useBoardStore();
-    const noteStore = useNoteStore();
-    const textWidgetStore = useTextWidgetStore();
-    const linkStore = useLinkStore();
-    const { updateItemPosition,calculateCenterPosition } = useItemManagement();
-    const imageStore = useImageStore();
-    const { loggedIn, clear } = useUserSession()
-// erro handler
-    const { triggerError } = useErrorHandler()
+  const boardStore = useBoardStore();
+  const noteStore = useNoteStore();
+  const textWidgetStore = useTextWidgetStore();
+  const linkStore = useLinkStore();
+  const { updateItemPosition, calculateCenterPosition } = useItemManagement();
+  const imageStore = useImageStore();
+  const { loggedIn, clear } = useUserSession();
+
+  // erro handler
+  const { triggerError } = useErrorHandler();
 
   const handlePaste = async (e: ClipboardEvent) => {
-    
     // Ignore if user is typing in an input field
     if (
       document.activeElement instanceof HTMLInputElement ||
       document.activeElement instanceof HTMLTextAreaElement ||
-      !(document.activeElement?.classList.contains('board'))
+      !document.activeElement?.classList.contains("board")
     ) {
       return;
     }
@@ -37,12 +37,12 @@ export function useClipboard() {
     if (!clipboardData) return;
 
     // Check for text
-    const text = clipboardData.getData('text');
+    const text = clipboardData.getData("text");
     if (text) {
       // Check if it's a URL
       try {
         const url = new URL(text);
-        if (url.protocol === 'http:' || url.protocol === 'https:') {
+        if (url.protocol === "http:" || url.protocol === "https:") {
           const position = calculateCenterPosition(400, 200);
           await linkStore.addLinkItem(text, {
             x: position.x,
@@ -58,29 +58,30 @@ export function useClipboard() {
 
       // If it's not a URL, add as a text widget or note
       if (text.length > 100) {
-        const position = calculateCenterPosition(300, 200,'note');
+        const position = calculateCenterPosition(300, 200, "note");
         noteStore.addNote(text, {
           x: position.x,
           y: position.y,
-          color: '#FFE589',
+          color: "#FFE589",
           width: 216,
           height: 216,
         });
       } else {
-        const position = calculateCenterPosition(300, 100,'textWidget');
-        textWidgetStore.addTextWidget({
-          x: position.x,
-          y: position.y,
-          width: 300,
-          height: 100,
-          
-        }).then((textWidget) => {
-          // You can modify the content here or do other tasks
-          textWidget.content.text = text;
-        })
-        .catch((error) => {
-          console.error('Error adding text widget:', error);
-        });
+        const position = calculateCenterPosition(300, 100, "textWidget");
+        textWidgetStore
+          .addTextWidget({
+            x: position.x,
+            y: position.y,
+            width: 300,
+            height: 100,
+          })
+          .then((textWidget) => {
+            // You can modify the content here or do other tasks
+            textWidget.content.text = text;
+          })
+          .catch((error) => {
+            console.error("Error adding text widget:", error);
+          });
       }
     }
 
@@ -89,22 +90,26 @@ export function useClipboard() {
     const items = clipboardData.items;
 
     const fileGroups = [...items].reduce((acc, item) => {
-      if(item.type.indexOf('image') !== -1) {
-        acc.image = acc.image ? [...acc.image, item.getAsFile()] : [item.getAsFile()];
+      if (item.type.indexOf("image") !== -1) {
+        acc.image = acc.image
+          ? [...acc.image, item.getAsFile()]
+          : [item.getAsFile()];
       }
-      return acc
-    } , {} as Record<string, any>)
+      return acc;
+    }, {} as Record<string, any>);
 
-    console.log(fileGroups)
-      if(fileGroups.image) {
-        if(loggedIn.value){
-          await imageStore.addImage(fileGroups.image);
-        }else{
-          triggerError({message: "Login to use Images and Files",title:"Login Required",onConfirm:redirectToLogin})
-         }
-      
+    console.log(fileGroups);
+    if (fileGroups.image) {
+      if (loggedIn.value) {
+        await imageStore.addImage(fileGroups.image);
+      } else {
+        triggerError({
+          message: "Login to use Images and Files",
+          title: "Login Required",
+          onConfirm: redirectToLogin,
+        });
+      }
     }
- 
   };
 
   // Helper function to calculate center position
@@ -116,7 +121,6 @@ export function useClipboard() {
   // };
 
   return {
-    handlePaste
+    handlePaste,
   };
 }
-
