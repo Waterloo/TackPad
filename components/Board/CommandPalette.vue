f8458fb094ce7463d07931b5568a9abed9c8101a
jithin <hello@jithinsha.in>
2025-04-05 17:55:58 +0530

    update shortcut

--- components/Board/CommandPalette.vue
+++ components/Board/CommandPalette.vue
@@ -20,19 +20,19 @@ const keys = useMagicKeys({
 });
 
 
-whenever(keys.alt_1, () => {
+whenever(keys.alt_shift_1, () => {
   if (!shouldIgnoreKeypress()) {
     addNote();
   }
 });
 
-whenever(keys.alt_2, () => {
+whenever(keys.alt_shift_2, () => {
   if (!shouldIgnoreKeypress()) {
     addTodoList();
   }
 });
 
-whenever(keys.alt_4, () => {
+whenever(keys.alt_shift_4, () => {
   if (!shouldIgnoreKeypress()) {
     addTimer();
   }
@@ -52,7 +52,7 @@ const commands = [
   { 
     id: 'new-note', 
     name: 'New Note', 
-    shortcut: 'Alt+1', 
+    shortcut: 'Alt+Shift+1', 
     category: 'Create', 
     action: () => {
       addNote();
@@ -61,7 +61,7 @@ const commands = [
   { 
     id: 'new-todo', 
     name: 'New Todo List', 
-    shortcut: 'Alt+2', 
+    shortcut: 'Alt+Shift+2', 
     category: 'Create', 
     action: () => {
       addTodoList();
@@ -70,7 +70,7 @@ const commands = [
   { 
     id: 'new-timer', 
     name: 'New Timer', 
-    shortcut: 'Alt+4', 
+    shortcut: 'Alt+Shift+4', 
     category: 'Create', 
     action: () => {
       addTimer();
@@ -79,7 +79,7 @@ const commands = [
   { 
     id: 'new-bookmark', 
     name: 'New Bookmark', 
-    shortcut: 'Alt+3', 
+    shortcut: 'Alt+Shift+3', 
     category: 'Create', 
     action: () => console.log('Creating new bookmark') 
   },
